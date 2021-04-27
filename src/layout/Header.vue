779472fc34917f578132d4f45df9f4140244b045
yi <13660339641@163.com>
2021-04-27 17:01:09 +0800

    nva

--- src/layout/Header.vue
+++ src/layout/Header.vue
@@ -11,12 +11,42 @@
         background-color="#545c64"
         text-color="#fff"
         active-text-color="#ffd04b">
-        <el-menu-item v-for="menu in navs" :key="menu.url" :index="menu.url">
-          {{menu.name}}
-        </el-menu-item>
+        <template v-for="menu in navs" :key="menu.url">
+          <el-submenu v-if="menu.children && menu.children.length > 0" :index="menu.url">
+            <template v-slot:title>{{menu.name}}</template>
+            <el-menu-item v-for="child in menu.children" :key="child.url" :index="child.url">{{child.name}}</el-menu-item>
+          </el-submenu>
+          <el-menu-item v-else :index="menu.url">{{menu.name}}</el-menu-item>
+        </template>        
       </el-menu>
     </div>
-    <i class="el-icon-s-operation navi-simple" />
+    <el-button type="text" @click="openDrawer">
+      <i class="el-icon-s-operation navi-simple" />
+    </el-button>
+    <el-drawer
+      v-model="drawer"
+      direction="ltr"
+      custom-class="drawer-style"
+      :with-header="false">
+      <el-menu
+        :default-active="defaultIndex"
+        :router="true"
+        class="el-menu-vertical-demo"
+        mode="vertical"
+        background-color="#545c64"
+        text-color="#fff"
+        style="border: none;"
+        @select="closeDrawer"
+        active-text-color="#ffd04b">
+        <template v-for="menu in navs" :key="menu.url">
+          <el-submenu v-if="menu.children && menu.children.length > 0" :index="menu.url">
+            <template v-slot:title>{{menu.name}}</template>
+            <el-menu-item v-for="child in menu.children" :key="child.url" :index="child.url">{{child.name}}</el-menu-item>
+          </el-submenu>
+          <el-menu-item v-else :index="menu.url">{{menu.name}}</el-menu-item>
+        </template>  
+      </el-menu>
+    </el-drawer>
     <personal-info />
   </div>
 </template>
@@ -25,7 +55,7 @@
 import { navs } from './_nav.js';
 import { useRoute } from 'vue-router';
 import PersonalInfo from './PersonalInfo.vue';
-import { ref } from 'vue';
+import { ref, watch } from 'vue';
 
 export default {
   components: {
@@ -35,10 +65,28 @@ export default {
     const route = useRoute();
 
     const defaultIndex = ref(route.path);
+    const drawer = ref(false);
+
+    const openDrawer = () => {
+      drawer.value = true;
+    };
+
+    watch(() => route.path, (nval, oval) => {
+      if (oval !== route.path) {
+        defaultIndex.value = route.path;
+      }
+    });
+
+    const closeDrawer = () => {
+      drawer.value = false;
+    };
 
     return {
       navs,
-      defaultIndex
+      defaultIndex,
+      drawer,
+      openDrawer,
+      closeDrawer
     };
   }
 };
@@ -68,6 +116,12 @@ export default {
     width: 0px;
     overflow: hidden;
     font-size: 40px;
+  }
+
+  .header:deep(.el-drawer) {
+    background-color: #545c64;
+    min-width: 160px;
+    max-width: 160px;
   }
 
   @media screen and (max-width: 768px) {
--- src/router/index.js
+++ src/router/index.js
@@ -19,11 +19,6 @@ const routes = [
     path: '/login',
     name: 'login',
     component: Login
-  },
-  {
-    path: '/about',
-    name: 'About',
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
   }
 ];
 
